c4076831f90aa63dc49b2d5e7e83231ee400acb5
sabaragamuwaSATD <it21836268@my.sliit.lk>
2024-10-02 11:33:59 +0530

    Fire Base and fire store Configuration changed

--- configs/FirebaseConfig.jsx
+++ configs/FirebaseConfig.jsx
@@ -1,23 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; 
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDlzZBy7SZLXPM26Bx3TWncEjqyotqbPjs",
-  authDomain: "mobileapp-7a44d.firebaseapp.com",
-  projectId: "mobileapp-7a44d",
-  storageBucket: "mobileapp-7a44d.appspot.com",
-  messagingSenderId: "689045379418",
-  appId: "1:689045379418:web:e034c7b4450249cdc1bb63",
-  measurementId: "G-CFT2WDKN1T"
+  apiKey: "AIzaSyAUge_0O3NA5qDOKRjtnQzI40Je1O-KEvo",
+  authDomain: "wastenet-59699.firebaseapp.com",
+  projectId: "wastenet-59699",
+  storageBucket: "wastenet-59699.appspot.com",
+  messagingSenderId: "77066363002",
+  appId: "1:77066363002:web:9edeba33f627d039c61bc3",
 };
-
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app);
-//const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const db = getFirestore(app);
+
+export { app, db };
