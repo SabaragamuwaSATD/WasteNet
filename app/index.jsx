942721fbe4c896dc75c60dd63c0a9ce907028177
sabaragamuwaSATD <it21836268@my.sliit.lk>
2024-10-07 22:15:01 +0530

    index path change to alll screen

--- app/index.jsx
+++ app/index.jsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return <Redirect href={"/User/UserHome"} />;
+  return <Redirect href={"/(tab)/home"} />;
 }
